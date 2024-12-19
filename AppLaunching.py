e8df754763e1752ffc07390359b0a171dd00e25e
satwik3002 <satwikkatamaraju@gmail.com>
2024-12-19 12:59:12 +0530

    MT

---
+++ AppLaunching.py
@@ -0,0 +1,28 @@
+import time
+
+from appium import webdriver
+from appium.webdriver.common.appiumby import AppiumBy
+
+# Step 1 : Create "Desired Capabilities"
+desired_caps = {}
+desired_caps['platformName'] = 'Android'
+desired_caps['automationName'] = 'UiAutomator2'
+desired_caps['platformVersion'] = '15'
+desired_caps['deviceName'] = 'emulator-5554'
+desired_caps['app'] = 'D:\\TestAPKs\\Android_Appium_Demo.apk'
+desired_caps['appPackage'] = 'com.skill2lead.appiumdemo'
+desired_caps['appActivity'] = 'com.skill2lead.appiumdemo.MainActivity'
+
+# Step 2 : Create "Driver object"
+#options = UiAutomator2Options().load_capabilities(desired_caps)
+driver = webdriver.Remote("http://localhost:4723/wd/hub",'desired_caps')
+
+# Step 3 : "Click on the App"
+ele_id = driver.find_element(AppiumBy.ID,"com.skill2lead.appiumdemo:id/EnterValue")
+ele_id.click()
+
+# Step 4 : Wait for 2 seconds
+time.sleep(2)
+
+# Step 5 : Close the driver object
+driver.quit()
